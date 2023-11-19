9019a352dc346fc0a9b6a4fbc558c41ed1546fec
zioscuro <lore.rang@gmail.com>
2023-11-19 19:28:04 +0100

    Add Clash Test Group section (work in progress)

--- src/main.ts
+++ src/main.ts
@@ -1,36 +1,39 @@
-import { downloadXml } from "./utils";
-import { writeXmlLC1, writeXmlLC2 } from "./clashXMLwriter";
-import { buildClashMatrix, resetClashMatrix } from "./clashMatrix";
-import { clashSelectionSetManager, selectionSetsArray } from "./clashSelectionSets";
+import { downloadXml } from './utils';
+import { writeXmlLC1, writeXmlLC2 } from './clashXMLwriter';
+import { buildClashMatrix, resetClashMatrix } from './clashMatrix';
+import {
+  clashSelectionSetManager,
+  selectionSetsArray,
+} from './clashSelectionSets';
 
 const btnExportLC1 = document.getElementById(
-  "btn-export-LC1"
+  'btn-export-LC1'
 ) as HTMLButtonElement;
 const btnExportLC2 = document.getElementById(
-  "btn-export-LC2"
+  'btn-export-LC2'
 ) as HTMLButtonElement;
 const btnGenerateClashMatrix = document.getElementById(
-  "btn-generate-clashmatrix"
+  'btn-generate-clashmatrix'
 ) as HTMLButtonElement;
 const btnRefreshClashMatrix = document.getElementById(
-  "btn-refresh-clashmatrix"
+  'btn-refresh-clashmatrix'
 ) as HTMLButtonElement;
 
 const clashGroupInput = document.getElementById(
-  "clash-group-input"
+  'clash-group-input'
 ) as HTMLInputElement;
 const clashGroupList = document.getElementById(
-  "clash-group-list"
+  'clash-group-list'
 ) as HTMLUListElement;
 const clashGroupAddBtn = document.getElementById(
-  "add-clash-group-input"
+  'add-clash-group-input'
 ) as HTMLButtonElement;
 
 const clashMatrixLC2 = document.getElementById(
-  "clashMatrix-LC2"
+  'clashMatrix-LC2'
 ) as HTMLTableElement;
 
-clashGroupAddBtn.addEventListener("click", (e: Event) => {
+clashGroupAddBtn.addEventListener('click', (e: Event) => {
   e.preventDefault();
 
   clashSelectionSetManager(clashGroupInput, clashGroupList);
@@ -38,17 +41,17 @@ clashGroupAddBtn.addEventListener("click", (e: Event) => {
   btnExportLC2.disabled = true;
 });
 
-btnGenerateClashMatrix.addEventListener("click", () => {
+btnGenerateClashMatrix.addEventListener('click', () => {
   buildClashMatrix(clashMatrixLC2, selectionSetsArray);
 
   btnGenerateClashMatrix.remove();
 
   btnRefreshClashMatrix.disabled = false;
-  btnRefreshClashMatrix.style.display = "block";
+  btnRefreshClashMatrix.style.display = 'block';
   btnExportLC2.disabled = false;
 });
 
-btnRefreshClashMatrix.addEventListener("click", () => {
+btnRefreshClashMatrix.addEventListener('click', () => {
   resetClashMatrix(clashMatrixLC2);
 
   buildClashMatrix(clashMatrixLC2, selectionSetsArray);
@@ -56,10 +59,57 @@ btnRefreshClashMatrix.addEventListener("click", () => {
   btnExportLC2.disabled = false;
 });
 
-btnExportLC1.addEventListener("click", () => {
-  downloadXml("fileXML-LC1", writeXmlLC1());
+btnExportLC1.addEventListener('click', () => {
+  downloadXml('fileXML-LC1', writeXmlLC1());
 });
 
-btnExportLC2.addEventListener("click", () => {
-  downloadXml("fileXML-LC2", writeXmlLC2(clashMatrixLC2));
+btnExportLC2.addEventListener('click', () => {
+  downloadXml('fileXML-LC2', writeXmlLC2(clashMatrixLC2));
 });
+
+// CLASH SECTIONS LIST (WORK IN PROGRESS)
+
+const clashSectionList = document.querySelector(
+  '#clash-section-list'
+) as HTMLElement;
+let currentClashSection = document.querySelector('.clash-section') as HTMLElement;
+
+const clashFirstSectionAddBtn = currentClashSection.querySelector('.add-section-btn') as HTMLButtonElement
+
+clashFirstSectionAddBtn?.addEventListener('click', () => {
+  addSection()
+  clashFirstSectionAddBtn.disabled = true
+});
+
+function addSection() {  
+  clashSectionList.appendChild(currentClashSection.cloneNode(true));
+
+  currentClashSection = clashSectionList.querySelector(
+    '.clash-section:last-child'
+  ) as HTMLButtonElement;
+
+  const addBtn = clashSectionList.querySelector(
+    '.clash-section:last-child .add-section-btn'
+  ) as HTMLButtonElement;
+  const removeBtn = clashSectionList.querySelector(
+    '.clash-section:last-child .remove-section-btn'
+  ) as HTMLButtonElement;
+
+  if (removeBtn.disabled) { removeBtn.disabled = false}
+
+  addBtn?.addEventListener('click', addSection);
+  removeBtn.addEventListener('click', removeSection);
+}
+
+function removeSection(e: MouseEvent) {
+  const removedBtn = e.target as HTMLButtonElement;
+  const removedSection = removedBtn.parentNode?.parentNode as HTMLElement;
+
+  clashSectionList.removeChild(removedSection)
+
+  const clashTestGroupCollection = document.querySelectorAll('.clash-section')
+
+  if (clashTestGroupCollection.length === 1) {
+    clashFirstSectionAddBtn.disabled = false
+  }
+}
