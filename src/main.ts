8f52ce1cc38b326b937423035785f7212490faa9
zioscuro <lore.rang@gmail.com>
2023-11-22 13:51:05 +0100

    remove feature/clash-stage-manager branch code

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,6 @@ import { downloadXml } from './utils';
 import { writeXmlLC1, writeXmlLC2 } from './clashXMLwriter';
 import { buildClashMatrix, resetClashMatrix } from './clashMatrix';
 import { clashSelectionSetManager, selectionSetsArray} from './clashSelectionSets';
-import { addSection } from './clashStageManager';
 
 const btnExportLC1 = document.getElementById(
   'btn-export-LC1'
@@ -65,18 +64,3 @@ btnExportLC2.addEventListener('click', () => {
   downloadXml('fileXML-LC2', writeXmlLC2(clashMatrixLC2));
 });
 
-// CLASH SECTIONS LIST (WORK IN PROGRESS)
-
-const clashSectionList = document.querySelector(
-  '#clash-section-list'
-) as HTMLElement;
-let currentClashStage = document.querySelector('.clash-section') as HTMLElement;
-
-const clashFirstSectionAddBtn = currentClashStage.querySelector(
-  '.add-section-btn'
-) as HTMLButtonElement;
-
-clashFirstSectionAddBtn?.addEventListener('click', () => {
-  addSection(clashSectionList, currentClashStage);
-  clashFirstSectionAddBtn.disabled = true;
-});
