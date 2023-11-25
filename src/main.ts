943606f80f34c2946e09dcbd0441f79f43f3ead3
zioscuro <lore.rang@gmail.com>
2023-11-25 13:41:46 +0100

    Remove LC1 and LC2 references in main.ts

--- src/main.ts
+++ src/main.ts
@@ -4,19 +4,6 @@ import { buildClashMatrix, resetClashMatrix } from './clashMatrix';
 import { clashSelectionSetManager, selectionSetsArray} from './clashSelectionSets';
 import { addSection } from './clashStageManager';
 
-const btnExportLC1 = document.getElementById(
-  'btn-export-LC1'
-) as HTMLButtonElement;
-const btnExportLC2 = document.getElementById(
-  'btn-export-LC2'
-) as HTMLButtonElement;
-const btnGenerateClashMatrix = document.getElementById(
-  'btn-generate-clashmatrix'
-) as HTMLButtonElement;
-const btnRefreshClashMatrix = document.getElementById(
-  'btn-refresh-clashmatrix'
-) as HTMLButtonElement;
-
 const clashGroupInput = document.getElementById(
   'clash-group-input'
 ) as HTMLInputElement;
@@ -27,42 +14,10 @@ const clashGroupAddBtn = document.getElementById(
   'add-clash-group-input'
 ) as HTMLButtonElement;
 
-const clashMatrixLC2 = document.getElementById(
-  'clashMatrix-LC2'
-) as HTMLTableElement;
-
 clashGroupAddBtn.addEventListener('click', (e: Event) => {
   e.preventDefault();
 
   clashSelectionSetManager(clashGroupInput, clashGroupList);
-
-  btnExportLC2.disabled = true;
-});
-
-btnGenerateClashMatrix.addEventListener('click', () => {
-  buildClashMatrix(clashMatrixLC2, selectionSetsArray);
-
-  btnGenerateClashMatrix.remove();
-
-  btnRefreshClashMatrix.disabled = false;
-  btnRefreshClashMatrix.style.display = 'block';
-  btnExportLC2.disabled = false;
-});
-
-btnRefreshClashMatrix.addEventListener('click', () => {
-  resetClashMatrix(clashMatrixLC2);
-
-  buildClashMatrix(clashMatrixLC2, selectionSetsArray);
-
-  btnExportLC2.disabled = false;
-});
-
-btnExportLC1.addEventListener('click', () => {
-  downloadXml('fileXML-LC1', writeXmlLC1());
-});
-
-btnExportLC2.addEventListener('click', () => {
-  downloadXml('fileXML-LC2', writeXmlLC2(clashMatrixLC2));
 });
 
 // CLASH SECTIONS LIST (WORK IN PROGRESS)
